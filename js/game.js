3822e792a0d17057f50a64532a40dcb53c89ad94
Craig Pilbin <craig.pilbin@gmail.com>
2014-04-27 16:45:51 +0100

    Particles and physics

--- js/game.js
+++ js/game.js
@@ -30,11 +30,13 @@ $(document).ready(function(){
 
 			this.game.load.image('healthBar', 'assets/healthBar.png');
 
+			this.game.load.image('emit', 'assets/emit.png');
+
 		},
 
 		create: function(){
 
-			this.player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
+			this.player = this.game.add.sprite(this.game.world.centerX, h / 3, 'player');
 
 			this.player.anchor.setTo(0.5,0.5);
 
@@ -52,8 +54,21 @@ $(document).ready(function(){
 
 			this.healthBar.scale.x = 200;
 
-			console.log(this.healthBar);
+			this.emit = game.add.emitter(this.game.world.centerX, h / 3 - 37, 250);
 
+			this.emit.makeParticles('emit', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30], 200, true, true);
+
+			this.emit.minParticleSpeed.setTo(-200, -300);
+		    
+		    this.emit.maxParticleSpeed.setTo(100, -400);
+		    
+		    this.emit.angularDrag = 50;
+
+		    this.emit.maxParticleScale = 1.3;
+
+		    this.emit.start(false, 800, 40);
+
+		    console.log(this.emit);
 		},
 
 		update: function(){
@@ -64,13 +79,19 @@ $(document).ready(function(){
 
 	        	this.bubble.body.velocity.y = -400;
 
+	        	this.emit.frequency = 5;
+
 	    	} else if(controls.up.isDown) {
 
 	        	this.bubble.body.velocity.y = -100;
 
+	        	this.emit.frequency = 150;
+
 	    	} else {
 
 	    		this.bubble.body.velocity.y = -200;
+
+	    		this.emit.frequency = 60;
 
 	    	}
 
@@ -78,11 +99,15 @@ $(document).ready(function(){
 
 		    	this.leanAnim(20);
 
+		    	this.emit.emitX = this.player.position.x + 10;
+
 		        this.player.body.velocity.x = -200;
 
 		    } else if(controls.right.isDown){
 
 		    	this.leanAnim(-20);
+
+		    	this.emit.emitX = this.player.position.x - 10;
 
 		        this.player.body.velocity.x = 200;
 
@@ -90,9 +115,13 @@ $(document).ready(function(){
 
 		    	this.leanAnim(0);
 
+		    	this.emit.emitX = this.player.position.x;
+		    
+		    	this.emit.emitY = this.player.position.y - this.player.height / 2;
+
 		    }
 
-		    this.healthBar.scale.x -= 0.5;
+			this.healthBar.scale.x -= 0.5;
 
 		    if(this.healthBar.scale.x <= 0){
 
